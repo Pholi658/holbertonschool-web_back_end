bc466269ce4f52a2f41e93afc4a3357e93d9a070
Pholi658 <totipholiso@gmail.com>
2026-04-26 20:59:23 +0200

    final updates

---
+++ Node_JS_basic/.history/1-stdin_20260426191127.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding('utf-8');
+
+console.log("Welcome to Holberton School, what is your name?\n")
+
+process.stdin.on('data', (data) => {
+    console.log("Your name is:", data);
+});
+
+process.stdin.on('end', () => {
+    console.log("This important software is now closing\n");
+});
